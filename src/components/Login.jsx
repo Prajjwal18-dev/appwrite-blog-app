be9aee998525cfe610477cceb9f514a15ac08ae8
Prajjwal Parashar <prajjwal.141820@gmail.com>
2025-09-03 14:57:00 +0530

    commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React , {useState} from "react";
+import { Link ,useNavigate } from "react-router-dom";
+import {login as authLogin} from "../store/authSlice"
+import Button from "../components/Header/Button"
+import {useDispatch} from "react-redux"
+import authService from "../appwrite/auth"
+import {useForm} from "react-hook-form"
+
+const Login = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {register , handleSubmit} = useForm();
+
+    const [error,setError] = useState("")
+
+    const login = async(data) =>{
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if(session){
+                const userData = await authService.getCurrentUser();
+                if(userData) dispatch(authLogin(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+  return <div>Login</div>;
+};
+
+export default Login;
